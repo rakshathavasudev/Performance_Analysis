97fe62953a849da6e27238ba90b4a350044bf6c8
Suhas <suhasbs.nitk@gmail.com>
2019-04-28 22:35:43 +0530

    Modified app and db added

--- Blog_App/flask_api.py
+++ Blog_App/flask_api.py
@@ -5,8 +5,11 @@ from flask_restful import reqparse
 from flaskext.mysql import MySQL
 import json
 import datetime
+from flask import jsonify
+import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-mysql = MySQL()
+# mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
@@ -15,40 +18,52 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'blog'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-mysql.init_app(app)
+# mysql.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+
+
+class PostsPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    content = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+
+
 api = Api(app)
 
 
-class CreateBlog(Resource):
-	def post(self):
-		try:
-			# Parse the arguments
-			parser = reqparse.RequestParser()
-			parser.add_argument('title', type=str, help='Name of the blog')
-			parser.add_argument('content', type=str, help='Content of the blog')
-			args = parser.parse_args()
+# class CreateBlog(Resource):
+# 	def post(self):
+# 		try:
+# 			# Parse the arguments
+# 			parser = reqparse.RequestParser()
+# 			parser.add_argument('title', type=str, help='Name of the blog')
+# 			parser.add_argument('content', type=str, help='Content of the blog')
+# 			args = parser.parse_args()
 
-			_blogname = args['title']
-			_content = args['content']
+# 			_blogname = args['title']
+# 			_content = args['content']
 		
 
-			conn = mysql.connect()
-			cursor = conn.cursor()
+# 			# conn = mysql.connect()
+# 			# cursor = conn.cursor()
 
-			cursor.callproc('spCreateBlog',(_blogname,_content,))
-			data = cursor.fetchall()
+# 			# cursor.callproc('spCreateBlog',(_blogname,_content,))
+# 			# data = cursor.fetchall()
 
-			if len(data) is 0:
-				conn.commit()
-				return {'StatusCode':'200','Message': 'Blog creation success'}
-			else:
-				return {'StatusCode':'1000','Message': str(data[0])}
+# 			if len(data) is 0:
+# 				conn.commit()
+# 				return {'StatusCode':'200','Message': 'Blog creation success'}
+# 			else:
+# 				return {'StatusCode':'1000','Message': str(data[0])}
 
 
-			#return {'Email': args['email'], 'Password': args['password'] }
+# 			#return {'Email': args['email'], 'Password': args['password'] }
 
-		except Exception as e:
-			return {'error': str(e)}
+# 		except Exception as e:
+# 			return {'error': str(e)}
 
 def convert_timestamp(item_date_object):
 	if isinstance(item_date_object, (datetime.date, datetime.datetime)):
@@ -57,38 +72,47 @@ def convert_timestamp(item_date_object):
 
 class GetBlog(Resource):
 	def get(self):
-		try:
-			# Parse the arguments
-			parser = reqparse.RequestParser()
-			parser.add_argument('title', type=str, help='Name of the blog')
-			#parser.add_argument('password', type=str, help='Password to create user')
-			args = parser.parse_args()
+		p = PostsPost.query.all()
+		posts = [{'title':p_i.title, 'content':p_i.content, 'created_at':p_i.created_at, 'updated_at':p_i.updated_at} for p_i in p]
+		# print(posts)
+		return jsonify(posts) 		
 
-			_blogname = args['title']
-			#_userPassword = args['password']
 
-			conn = mysql.connect()
-			cursor = conn.cursor()
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('title', type = str,)
+		parser.add_argument('content', type = str)
+		args = parser.parse_args()
+		print(args)
+		p = PostsPost(title=args['title'], content=args['content'])
+		db.session.add(p)
+		db.session.commit()
+		json = {}
+		json['title'] = p.title
+		json['content'] = p.content
+		json['created_at'] = p.created_at
+		json['updated_at'] = p.updated_at
+		return jsonify(json)
 
-			cursor.callproc('spGetBlogbyname',(_blogname,))
-			data = cursor.fetchall()
+class GetBlogList(Resource):
+
+	def get(self, id):
+		
+		p = PostsPost.query.filter_by(id=int(id))[0]
+		print(p.__dict__)
+		json = {}
+		json['title'] = p.title
+		json['content'] = p.content
+		json['created_at'] = p.created_at
+		json['updated_at'] = p.updated_at
+		return jsonify(json)
+
 	
-					
-			if (len(data)>0):
-				with open("data_file.json", "w") as write_file:
-					
-					json.dump(data, write_file,default=convert_timestamp)
-					#print(data)
-					return {'StatusCode':'200','Message':'Blog exists'}
-			else:
-				return {'StatusCode':'1000','Message': 'Blog not found'}
 
-		except Exception as e:
-			return {'error': str(e)}
+api.add_resource(GetBlog, '/api/')
+api.add_resource(GetBlogList, '/api/<int:id>/')
 
-
-api.add_resource(GetBlog, '/GetBlog')
-api.add_resource(CreateBlog, '/CreateBlog')
+# api.add_resource(CreateBlog, '/CreateBlog')
 
 if __name__ == '__main__':
 	app.run(debug=True)
